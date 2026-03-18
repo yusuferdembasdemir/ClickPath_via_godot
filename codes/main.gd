15ab1ad64718ead0e47c82cbf6be245dfde8bab2
yusuferdembasdemir <yusuferdembasdemir@gmail.com>
2026-03-19 02:06:40 +0300

    Shop Update part 1/2

--- codes/main.gd
+++ codes/main.gd
@@ -3,6 +3,9 @@ extends Node2D
 # Current click count,do this 'more and moreee'
 var click_count = 0
 
+func _ready():
+	update_label()
+	
 func _on_button_pressed():
 	# Store the current digit count 
 	var previous_digit_count = str(click_count).length()
@@ -17,3 +20,11 @@ func _on_button_pressed():
 	# If digit count increased, move the UI to the left
 	if current_digit_count > previous_digit_count:
 		$Label.position.x -= 15
+		
+func update_label():
+	$Label.text = "Clicks: " + str(click_count)
+
+
+func _on_shopbutton_pressed() -> void:
+	#Doin this in 1.30 am n I am so tired,this code goes to shop part
+	get_tree().change_scene_to_file("res://scenes/shopsceene.tscn")
