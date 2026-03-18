018fd2cbe633ad84d6643fceb00d7f2f1e4527a2
yusuferdembasdemir <yusuferdembasdemir@gmail.com>
2026-03-19 00:27:38 +0300

    First Version

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Current click count,do this 'more and moreee'
+var click_count = 0
+
+func _on_button_pressed():
+	# Store the current digit count 
+	var previous_digit_count = str(click_count).length()
+	
+	# Increment the count
+	click_count += 1
+	$Label.text = "Clicks: " + str(click_count)
+	
+	# Check the new digit count (e.g., 10 is 2 digits)
+	var current_digit_count = str(click_count).length()
+	
+	# If digit count increased, move the UI to the left
+	if current_digit_count > previous_digit_count:
+		$Label.position.x -= 15
